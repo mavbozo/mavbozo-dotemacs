23127173d11f16f326954c3ab508a4ed2e980737
Avicenna <maverickbozo@me.com>
2014-09-22 23:16:56 +0700

    web-mode's file extension invocation

--- init.el
+++ init.el
@@ -198,3 +198,7 @@
 ;; DATOMIC SNIPPETS
 (add-to-list 'load-path "~/.emacs.d/vendor/datomic-snippets")
 (require 'datomic-snippets)
+
+
+;; WEB-MODE
+(require 'web-mode) (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode)) (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
