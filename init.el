a93ee5a5edd94d172fd314cf5c56b9e60cd82934
Avicenna <maverickbozo@me.com>
2014-12-16 19:05:16 +0700

    add helm as submodule

--- init.el
+++ init.el
@@ -1,3 +1,34 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(package-initialize)
+
+;; Install package when not installed
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; My default package
+(defvar my-packages '(ergoemacs-mode helm clojure-mode clojure-snippets company magit dash rainbow-delimiters cider markdown-mode php-mode web-mode s)
+  "A list of packages to ensure are installed at launch.")
+
+;; Install those default packages if not yet installed
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/zenburn-emacs")
+
+;; load tabbar
+(add-to-list 'load-path "~/.emacs.d/vendor/tabbar")
+(require 'tabbar)
+
+;; ergoemacs mode
+(require 'ergoemacs-mode)
+(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+
+;; visual line mode
+(global-visual-line-mode 1)
 (load "~/.emacs.d/mavbozo-custom-head")
 
 (custom-set-variables
